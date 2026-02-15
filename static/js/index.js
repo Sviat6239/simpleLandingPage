3806758d5337d29fb31e83f11f02480a7ff0b4cc
Sviat6239 <sviatoslav6239@gmail.com>
2026-02-15 14:24:08 +0100

    added basic list of consts

---
+++ static/js/index.js
@@ -0,0 +1,36 @@
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
+const ts = document.querySelector('#ts');
+
+const djang = document.querySelector('#django');
+const flask = document.querySelector('#flask');
+const falcon = document.querySelector('#falcon');
+
+const nodejs = document.querySelector('#nodejs');
+const fastify = document.querySelector('#fastify');
+const express = document.querySelector('#express');
+
+const c = document.querySelector('#c');
+const d = document.querySelector('#d');
+const cpp = document.querySelector('#c++');
+const csharp = document.querySelector('#c#');
+const lua = document.querySelector('#lua');
+const python = document.querySelector('#python');
+const nodejsGeneralDev = document.querySelector('#nodejs-generaldev');
+
+const fasm = document.querySelector('#fasm');
+const nasm = document.querySelector('#nasm');
+const tasm = document.querySelector('#tasm');
+const rv32 = document.querySelector('#rv32');
+const i8080 = document.querySelector('#i8080');
+const z80 = document.querySelector('#z80');
+
+const linux = document.querySelector('#linux');
+const proxmox = document.querySelector('#proxmox');
+const windows = document.querySelector('windows');
+const linuxServer = document.querySelector('#linuxserver');
+const windowsServer = document.querySelector('#windowsServer');
+const bash = document.querySelector('#bash');
+const powerShell = document.querySelector('#powershell');
+
