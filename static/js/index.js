52860ad6d7d8d47c5188cc0e2c4e1d920f5f0a4f
Sviat6239 <sviatoslav6239@gmail.com>
2026-02-18 18:07:32 +0100

    added some plates with lvls

--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,63 @@
-const html = document.querySelector('#html');
-const css = document.querySelector('#css');
-const js = document.querySelector('#js');
-const ts = document.querySelector('#ts');
-const quickjs = document.querySelector('#quickjs');
-const react = document.querySelector('#react');
-const svelte = document.querySelector('#svelte');
-const solidjs = document.querySelector('#solidjs');
+document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('#html');
+    const css = document.querySelector('#css');
+    const js = document.querySelector('#js');
+    const ts = document.querySelector('#ts');
+    const quickjs = document.querySelector('#quickjs');
+    const react = document.querySelector('#react');
+    const svelte = document.querySelector('#svelte');
+    const solidjs = document.querySelector('#solidjs');
 
-const djang = document.querySelector('#django');
-const flask = document.querySelector('#flask');
-const falcon = document.querySelector('#falcon');
+    const djang = document.querySelector('#django');
+    const flask = document.querySelector('#flask');
+    const falcon = document.querySelector('#falcon');
 
-const nodejs = document.querySelector('#nodejs');
-const bun = document.querySelector('#bun');
-const fastify = document.querySelector('#fastify');
-const express = document.querySelector('#express');
-const elysiajs = document.querySelector('#elysiajs');
+    const nodejs = document.querySelector('#nodejs');
+    const bun = document.querySelector('#bun');
+    const fastify = document.querySelector('#fastify');
+    const express = document.querySelector('#express');
+    const elysiajs = document.querySelector('#elysiajs');
 
-const c = document.querySelector('#c');
-const d = document.querySelector('#d');
-const cpp = document.querySelector('#c++');
-const csharp = document.querySelector('#c#');
-const lua = document.querySelector('#lua');
-const python = document.querySelector('#python');
-const nodejsGeneralDev = document.querySelector('#nodejs-generaldev');
+    const c = document.querySelector('#c');
+    const d = document.querySelector('#d');
+    const cpp = document.querySelector('#cpp');
+    const csharp = document.querySelector('#csharp');
+    const lua = document.querySelector('#lua');
+    const python = document.querySelector('#python');
+    const nodejsGeneralDev = document.querySelector('#nodejs-generaldev');
 
-const fasm = document.querySelector('#fasm');
-const nasm = document.querySelector('#nasm');
-const tasm = document.querySelector('#tasm');
-const rv32 = document.querySelector('#rv32');
-const i8080 = document.querySelector('#i8080');
-const z80 = document.querySelector('#z80');
+    const fasm = document.querySelector('#fasm');
+    const nasm = document.querySelector('#nasm');
+    const tasm = document.querySelector('#tasm');
+    const masm = document.querySelector('#masm');
+    const rv32 = document.querySelector('#rv32');
+    const i8080 = document.querySelector('#i8080');
+    const z80 = document.querySelector('#z80');
 
-const linux = document.querySelector('#linux');
-const proxmox = document.querySelector('#proxmox');
-const windows = document.querySelector('windows');
-const linuxServer = document.querySelector('#linuxserver');
-const windowsServer = document.querySelector('#windowsServer');
-const bash = document.querySelector('#bash');
-const powerShell = document.querySelector('#powershell');
-const git = document.querySelector('#git');
-const github = document.querySelector('#github');
-const vscode = document.querySelector('#vscode');
+    const linux = document.querySelector('#linux');
+    const proxmox = document.querySelector('#proxmox');
+    const windows = document.querySelector('#windows');
+    const linuxServer = document.querySelector('#linuxserver');
+    const windowsServer = document.querySelector('#windowsServer');
+    const bash = document.querySelector('#bash');
+    const powerShell = document.querySelector('#powershell');
+    const git = document.querySelector('#git');
+    const github = document.querySelector('#github');
+    const vscode = document.querySelector('#vscode');
 
+
+    html.innerHTML = 'HTML <span class="badge" id="advanced">Advansed</span>';
+    css.innerHTML = 'CSS <span class="badge" id = "intermediate">Intermediate</span>';
+    js.innerHTML = 'JS <span class="badge" id="basic">Basic</span>';
+    ts.innerHTML = 'TS <span class="badge" id="elementary">Elementary</span>';
+    quickjs.innerHTML = 'Quick.JS <span class="badge" id="begginer">Begginer</span>';
+    react.innerHTML = 'React <span class="badge" id="begginer">Begginer</span>';
+    svelte.innerHTML = 'Svelte <span class="badge" id="begginer">Begginer</span>';
+    solidjs.innerHTML = 'Solid.JS <span class="badge" id="begginer">Begginer</span>';
+
+    django.innerHTML = 'Django <span class="badge" id="intermediate">Intermediate</span>';
+    flask.innerHTML = 'Flask <span class="badge" id="advanced">Adnaced</span>';
+    falcon.innerHTML = 'Falcon <span class="badge" id="elementary">Elementary</span>';
+
+
+})
