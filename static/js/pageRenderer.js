0e8a21da0f5e769e74a2352529beee36e63b63fd
Sviat6239 <sviatoslav6239@gmail.com>
2026-02-27 15:25:06 +0100

    added buttons div to navigate between pages

---
+++ static/js/pageRenderer.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const projectsDivAsButton = document.querySelectorAll('.projects');
+    const generalPage = document.querySelector('.card');
+
+    projectsDivAsButton.forEach(div => {
+        div.addEventListener('click', () => {
+
+        });
+    });
+
+
+})
